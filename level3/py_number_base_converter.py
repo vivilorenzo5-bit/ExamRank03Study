78fba7faee327de62557dabf0b54ec658d4d27b1
Vicente Lourenco Prista Von Bonhorst <vlourenc@c2r7s2.42lisboa.com>
2026-09-14 10:35:46 +0100

    done

--- level3/py_number_base_converter.py
+++ level3/py_number_base_converter.py
@@ -25,3 +25,14 @@ def number_base_converter(number: str, from_base: int, to_base: int) -> str:
         res = digits[remainder] + res
         decimal_val //= to_base
     return res
+
+
+if __name__ == "__main__":
+    print(number_base_converter("1010", 2, 10))   # "10"
+    print(number_base_converter("FF", 16, 10))    # "255"
+    print(number_base_converter("255", 10, 16))   # "FF"
+    print(number_base_converter("123", 10, 2))    # "1111011"
+    print(number_base_converter("Z", 36, 10))     # "35"
+    print(number_base_converter("35", 10, 36))    # "Z"
+    print(number_base_converter("123", 1, 10))    # "ERROR"
+    print(number_base_converter("G", 16, 10))     # "ERROR"
